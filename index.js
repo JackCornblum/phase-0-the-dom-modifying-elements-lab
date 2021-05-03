bfa1150a984c7a80e2aee63344126061ef038868
jackcornblum <jcornblum@gmail.com>
2021-05-03 12:53:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+const newContent = "Jack is the champion";
+newHeader.innerHTML = newContent;
+
+document.body.appendChild(newHeader, "victory");
